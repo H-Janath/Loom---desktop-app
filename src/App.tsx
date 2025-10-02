b510c0ea8921980461dfd40e9ceda59822b87e67
H-Janath <janathhma@gmail.com>
2025-10-02 12:16:10 +0530

    Initital-commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <Button>
+          CLick
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
